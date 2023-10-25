52a158a4acec090488bc704084fcdafb4d6af6ce
Lenkola Sampo <sampo.lenkola@aalto.fi>
2023-10-25 14:31:57 +0300

    Puhelinluettelo valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,6 +3,8 @@ import Filter from './Filter'
 import Form from './Form'
 import Search from './Search'
 import noteService from './services/notes'
+import './style.css'
+
 
 const App = () => {
 
@@ -13,6 +15,8 @@ const App = () => {
   const [search, setSearch] = useState('')
 
   const [person, setPerson] = useState([])
+
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const hook = () => {
     noteService.getAll()
@@ -46,15 +50,51 @@ const App = () => {
             setPerson(person.map(p => p.id === existingPerson.id ? x.data : p));
             setNewName('');
             setNewNumber(''); })
-      }
-      
+          .catch(error => {
+            setErrorMessage(`Information of ${newName} has already been removed from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+          }, 5000);
+          
+          })
+          }
+    
     } else {
       setPerson(person.concat(newPerson))
       noteService.create(newPerson)
+      setErrorMessage(
+        `Added ${newPerson.name}`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+
       setNewName('')
       setNewNumber('')
     }
   }
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+        return null
+    }
+
+    let className = 'notification';
+
+    if (message.startsWith("Added")) {
+        className += ' success';
+    } else {
+        className += ' error';
+    }
+
+    return (
+        <div className={className}>
+            {message}
+        </div>
+    )
+}
+
+
 
   const handleSearch = (event) => {
     setSearch(event.target.value)
@@ -78,6 +118,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Search search={search} handleSearch={handleSearch}/>
       <h3>add a new</h3>
       <Form 
